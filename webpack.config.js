62e2b1df1202957674c12d901805a6e4fcfee8cb
Krittayot007 <krittayot.som@gmail.com>
2023-04-20 11:58:49 +0700

    add patch 1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[hash].js',
+    entry: {
+        main: './src/index.js',
+        vendor: './src/vendor.js',
     },
 
     // LOADER 
     module: {
         rules: [
-            {
-                test: /\.s?css$/i, // เฉพาะกับ .css .scss
-                use: ['style-loader', 'css-loader', 'sass-loader'], // ใช้ loader อะไรบ้าง
-            },
             {
                 test: /\.html$/i,
                 loader: "html-loader",
@@ -24,11 +15,5 @@ module.exports = {
         ],
     },
 
-    // PLUGIN
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-        template: './src/template/index.html',
-        }),
-    ],
+   
 };
---
+++ webpack.prod.js
@@ -0,0 +1,57 @@
+// IMPORT:Other Module
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// IMPORT:Our Module
+const commonConfig = require('./webpack.config');
+
+// #################################
+const devConfig = merge(commonConfig, {
+    mode : 'production',
+
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: '[name].[hash].min.js', // .min = minify == remove all code comment and write space
+        assetModuleFilename: 'images/[hash][ext][query]',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/i, // เฉพาะกับ .css .scss
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'], // ใช้ loader อะไรบ้าง
+            },
+        ],
+    },
+
+     // PLUGIN
+     plugins: [
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+        template: './src/template/index.html',
+        }),
+    ],
+
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/template/index.html',
+                filename: 'index.min.html',
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true,
+                },
+            }),
+        ],
+    },
+})
+
+module.exports = devConfig;
